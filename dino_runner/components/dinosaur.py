c9957734cccdab9406b38f834a653aadcee40222
KaykyArthur <128011123+KaykyArthur@users.noreply.github.com>
2023-03-17 02:05:19 -0300

    Adicionado: Duck ou Agachar (EXERCICIO)
    - o dino tanto abaixado quanto levantado permanece na "mesma" altura de pixel;
    - o dino não consegue pular se estiver agachado, para reproduzir a mesma jogabiidade da versão original;

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,9 +1,10 @@
 import pygame
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
-Y_POS = 310
+Y_POS = 312
+Y_POSDUCK = 347
 JUMP_VEL = 8.5
 
 class Dinosaur:
@@ -12,6 +13,7 @@ class Dinosaur:
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
+        self.dino_duck = False
         self.dino_run = True
         self.dino_jump = False
         self.step_index = 0
@@ -32,23 +34,43 @@ class Dinosaur:
         if self.dino_jump:
             self.dino_rect.y -= self.jump_vel*4
             self.jump_vel -=0.8
-
+ 
         if self.jump_vel < -JUMP_VEL:
             self.dino_jump = False
             self.dino_rect.y = Y_POS
             self.jump_vel = JUMP_VEL
     
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POSDUCK
+        self.step_index+=1        
+        
+        if self.step_index >= 10:
+            self.step_index = 0
+        
+    
+    
     def update(self, user_input):
-        if user_input[pygame.K_UP] and not self.dino_jump:
+        if user_input[pygame.K_UP] and not self.dino_jump and not self.dino_duck:
             self.dino_jump = True
             self.dino_run = False
         elif not self.dino_jump:
             self.dino_run = True
-            
+
+        if user_input[pygame.K_DOWN]:
+            self.dino_duck = True
+            self.dino_run = False
+        else:
+            self.dino_duck = False
+        
         if self.dino_run:
             self.run()
         elif self.dino_jump:
-            self.jump()                
+            self.jump()  
+        elif self.dino_duck:
+            self.duck()            
             
     
     def draw(self, screen):
